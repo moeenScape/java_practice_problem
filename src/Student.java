b05be1980c0496886c7d30f64fe9c8774a84e046
moeen775 <moeenahmmed2015br@gmail.com>
2024-01-02 23:54:32 +0600

    Stream APi Problem Solve

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,7 @@
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Student {
     private int id;
@@ -29,7 +31,7 @@ public class Student {
     public static void main(String[] args) {
         List<Student> studentList = List.of(
                 new Student(1, "Moeen", 24, "Male", "CSE", "Dhaka", 50, List.of("01718008604", "01718008605")),
-                new Student(2, "John", 22, "Male", "Math", "New York", 40, List.of("01718008606", "01718008607")),
+                new Student(2, "John", 22, "Male", "CSE", "New York", 40, List.of("01718008606", "01718008607")),
                 new Student(3, "Alice", 23, "Female", "Physics", "Dhaka", 45, List.of("01718008618", "01718008619")),
                 new Student(4, "Bob", 25, "Male", "Chemistry", "Paris", 35, List.of("01718008620", "01718008621")),
                 new Student(5, "Eva", 21, "Female", "Biology", "Berlin", 55, List.of("01718008622", "01718008623")),
@@ -64,8 +66,39 @@ public class Student {
 //        studentByCity.forEach(System.out::println);
 
 //        Q3. Find all Department Name
-        List<String> deptName = studentList.stream().map(Student::getDept).sorted().toList();
-        deptName.forEach(System.out::println);
+        List<String> deptName = studentList.stream().map(Student::getDept).sorted().distinct().toList();
+//        deptName.forEach(System.out::println);
+
+//      Q4 . find all Contact Number
+        List<List<String>> studentContact = studentList.stream()
+                .map(Student::getContacts).toList();
+
+        List<String> contactList = studentList.stream()
+                .flatMap(student -> student.getContacts().stream()).toList();
+
+//        contactList.forEach(System.out::println);
+
+
+//        Q5. Group The Student By Department
+
+        Map<String, List<Student>> collect =
+                studentList.stream().collect(Collectors.groupingBy(Student::getDept));
+
+        System.out.println(collect);
+
+        Map<String, Long> countStudent =
+                studentList.stream().collect(Collectors.groupingBy(Student::getDept,Collectors.counting()));
+
+        System.out.println(countStudent);
+
+//        Q6 Find Which department have more student
+
+        Map.Entry<String, Long> maxDept =
+                studentList.stream()
+                        .collect(Collectors.groupingBy(Student::getDept,Collectors.counting()))
+                        .entrySet().stream().max(Map.Entry.comparingByValue()).get();
+
+        System.out.println(maxDept);
 
     }
 
